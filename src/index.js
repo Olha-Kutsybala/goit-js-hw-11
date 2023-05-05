c698fbe470fdd589b5115b2f33b20b9fbc180d0c
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-05-05 17:20:05 +0300

    pagination

--- src/index.js
+++ src/index.js
@@ -6,80 +6,36 @@
 // import { renderGallery } from './js/renderGallery';
 // import { scrolling } from './js/scroll';
 // import { totalPage } from './js/fetchImages';
-
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 import { onFormSubmit } from './js/handler';
 import { refs } from './js/refs';
+import { options } from './js/paginationOptions';
 
 refs.form.addEventListener('submit', onFormSubmit);
+
+const container = document.getElementById('pagination');
+const pagination = new Pagination(container, options);
+
+pagination.on('afterMove', event => {
+  const currentPage = event.page;
+  console.log(currentPage);
+});
+
+pagination.on('beforeMove', event => {
+  const currentPage = event.page;
+
+  if (currentPage === 10) {
+    return false;
+    // return true;
+  }
+});
 
 // refs.btnLoadMore.addEventListener('click', onLoadMoreBtnClick);
 
 // let query = '';
 // let numberOfPage = 1;
 // export const per_page = 40;
-
-// const lightbox = new SimpleLightbox('.gallery a');
-
-// function onFormSubmit(evt) {
-//   evt.preventDefault();
-//   numberOfPage = 1;
-//   refs.gallery.innerHTML = '';
-//   refs.btnLoadMore.classList.add('hidden');
-//   const { searchQuery } = evt.target.elements;
-
-//   if (!searchQuery.value.trim()) {
-//     Notify.failure('Enter the query in the search!');
-//     return;
-//   }
-
-//   query = searchQuery.value;
-//   addGallery();
-//   evt.currentTarget.reset();
-// }
-
-// async function addGallery() {
-//   try {
-//     const { hits, totalHits } = await fetchImages(query, numberOfPage);
-
-//     if (!hits.length) {
-//       refs.gallery.innerHTML = '';
-//       Notify.failure(
-//         'Sorry, there are no images matching your search query. Please try again.'
-//       );
-//     } else {
-//       renderGallery(hits);
-//       Notify.success(`Hooray! We found ${totalHits} images.`);
-//       lightbox.refresh();
-//       if (totalPage > 1) {
-//         refs.btnLoadMore.classList.remove('hidden');
-//       }
-//     }
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// export async function addMoreImages() {
-//   try {
-//     const { hits } = await fetchImages(query, numberOfPage);
-//     renderGallery(hits);
-//     lightbox.refresh();
-//     scrolling();
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// function onLoadMoreBtnClick(evt) {
-//   numberOfPage += 1;
-//   if (numberOfPage > totalPage) {
-//     evt.target.classList.add('hidden');
-//     Notify.failure(
-//       "We're sorry, but you've reached the end of search results."
-//     );
-//   }
-//   addMoreImages();
-// }
 
 // const lightbox = new SimpleLightbox('.gallery a');
 
--- src/js/handler.js
+++ src/js/handler.js
@@ -7,34 +7,34 @@ import { checkTotalRenderImage } from './checkTotalRenderImage';
 import { refs } from './refs';
 import { observer, scrolling } from './scrollFunction';
 
-export async function onLoadMoreBtnClick(evt) {
-  try {
-    if (refs.checkbox.checked) {
-      observer.observe(refs.observerTarget);
-      loadMoreBtn.hide();
-    } else {
-      loadMoreBtn.loading();
-    }
+// export async function onLoadMoreBtnClick(evt) {
+//   try {
+//     if (refs.checkbox.checked) {
+//       observer.observe(refs.observerTarget);
+//       loadMoreBtn.hide();
+//     } else {
+//       loadMoreBtn.loading();
+//     }
 
-    setTimeout(async () => {
-      const { hits, totalHits } = await imageApiService.fetchImages();
-      renderGalleryMarkup(hits);
-      if (!refs.checkbox.checked) {
-        loadMoreBtn.endloading();
-      }
-      checkTotalRenderImage(totalHits);
-    }, 1000);
-    lightbox.refresh();
-  } catch (error) {
-    console.log(error);
-  }
-  scrolling();
-}
+//     setTimeout(async () => {
+//       const { hits, totalHits } = await imageApiService.fetchImages();
+//       renderGalleryMarkup(hits);
+//       if (!refs.checkbox.checked) {
+//         loadMoreBtn.endloading();
+//       }
+//       checkTotalRenderImage(totalHits);
+//     }, 1000);
+//     lightbox.refresh();
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   scrolling();
+// }
 
 export async function onFormSubmit(evt) {
   evt.preventDefault();
   clearGalleryMarkup();
-  loadMoreBtn.hide();
+  // loadMoreBtn.hide();
   imageApiService.resetPage();
 
   const searchQuery = evt.target.searchQuery.value.trim();
@@ -42,7 +42,7 @@ export async function onFormSubmit(evt) {
   if (!searchQuery) {
     Notify.failure('Enter the query in the search!');
     clearGalleryMarkup();
-    loadMoreBtn.hide();
+    // loadMoreBtn.hide();
     observer.unobserve(refs.observerTarget);
     return;
   }
@@ -56,7 +56,7 @@ export async function onFormSubmit(evt) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-      loadMoreBtn.hide();
+      // loadMoreBtn.hide();
       return;
     }
     if (hits.length > 0) {
@@ -67,7 +67,7 @@ export async function onFormSubmit(evt) {
     if (refs.checkbox.checked) {
       observer.observe(refs.observerTarget);
     } else {
-      loadMoreBtn.show();
+      // loadMoreBtn.show();
     }
 
     checkTotalRenderImage(totalHits);
